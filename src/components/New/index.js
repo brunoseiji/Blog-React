7c547759b305cd7063e6edf3ffb21fa6de674f49
Bruno Seiji <bruno_seiji_1995@hotmail.com>
2020-03-05 18:05:39 -0300

    Finalizando pagina de criação de posts

--- src/components/New/index.js
+++ src/components/New/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Link, withRouter} from 'react-router-dom';
+import firebase from '../../firebase';
 import './new.css';
 
 class New extends Component {
@@ -9,13 +10,36 @@ class New extends Component {
         this.state = {
             titulo: '',
             imagem: '',
-            descricao: ''
+            descricao: '',
+            alert: ''
         };
         this.cadastrar = this.cadastrar.bind(this);
     }
 
-    cadastrar() {
+    componentDidMount() {
+        if(!firebase.getCurrent()) {
+            this.props.history.replace('/')
+            return null;
+        }
+    }
 
+    cadastrar = async(e) => {
+        e.preventDefault()
+        if(this.state.titulo !== '' && this.state.imagem !== '' && this.state.descricao !== '') {
+            let posts = firebase.app.ref('posts');
+            let chave = posts.push().key;
+
+            await posts.child(chave).set({
+                titulo: this.state.titulo,
+                imagem: this.state.imagem,
+                descricao: this.state.descricao,
+                autor: localStorage.nome
+            });
+
+            this.props.history.push('/dashboard');
+        } else {
+            this.setState({alert: 'Preencha todos os campos!'});
+        }
     }
 
     render(){
@@ -25,6 +49,7 @@ class New extends Component {
                     <Link to="/dashboard">Voltar</Link>
                 </header>
                 <form onSubmit={this.cadastrar} id="new-post">
+                    <span>{this.state.alert}</span>
                     <label>Título:</label><br/>
                     <input type="text" placeholder="Nome do Post" value={this.state.titulo} autoFocus
                     onChange={(e) => this.setState({titulo: e.target.value})} /> <br/>
